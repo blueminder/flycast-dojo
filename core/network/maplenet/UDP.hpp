087d91a542eda822f0fad457cf7a9107c49a1380
Enrique Santos <me@eos.fyi>
2020-12-01 00:01:34 -0800

    adjust scope in udp client

--- core/network/maplenet/UDP.hpp
+++ core/network/maplenet/UDP.hpp
@@ -11,6 +11,10 @@ private:
 	int port;
 	bool isLoopStarted;
 
+	std::map<int, long> ping_send_ts;
+	std::vector<long> ping_rtt;
+	int avg_ping_ms;
+
 	std::atomic<bool> write_out;
 	unsigned char to_send[256];
 
@@ -22,24 +26,20 @@ private:
 	sock_t local_socket = INVALID_SOCKET;
 	void CloseSocket(sock_t& socket) const { closesocket(socket); socket = INVALID_SOCKET; }
 
-public:
-	UDPClient();
-
 	sock_t CreateAndBind(int port);
 	bool CreateLocalSocket(int port);
 
-	void SetHost(std::string host, int port);
 	bool Init(bool hosting);
-
 	void ClientLoop();
 
-	bool disconnect_toggle;
-	bool opponent_disconnected;
-
+public:
+	UDPClient();
 	void ClientThread();
 
-	int SendData(std::string data);
-	void SendMsg(std::string msg, sockaddr_in target);
+	void StartSession();
+	void EndSession();
+
+	void SetHost(std::string host, int port);
 
 	int PingAddress(const char * ip_addr, int port, int add_to_seed);
 	int PingAddress(sockaddr_in target_addr, int add_to_seed);
@@ -47,19 +47,17 @@ public:
 	int GetAvgPing(const char * ip_addr, int port);
 	int GetOpponentAvgPing();
 
+	int SendData(std::string data);
+	void SendMsg(std::string msg, sockaddr_in target);
 	void SendPlayerName();
 	void SendNameOK();
 
 	void SendDisconnect();
 	void SendDisconnectOK();
 
+	bool disconnect_toggle;
+	bool opponent_disconnected;
+
 	bool name_acknowledged;
-
-	void StartSession();
-	void EndSession();
-
-	std::map<int, long> ping_send_ts;
-	std::vector<long> ping_rtt;
-	int avg_ping_ms;
 };
 
