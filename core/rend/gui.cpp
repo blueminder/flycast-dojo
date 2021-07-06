8abfa28effb0c2bb890fc5157eed88a84ce4a553
Enrique Santos <me@eos.fyi>
2021-07-06 04:17:35 +0300

    remove interactive RECEIVE option in gui

--- core/rend/gui.cpp
+++ core/rend/gui.cpp
@@ -1984,9 +1984,9 @@ static void gui_display_content()
 	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(12 * scaling, 6 * scaling));		// from 8, 4
 	ImGui::AlignTextToFramePadding();
 	static ImGuiComboFlags flags = 0;
-	const char* items[] = { "OFFLINE", "HOST", "JOIN", "TRAIN", "RECEIVE"};
+	const char* items[] = { "OFFLINE", "HOST", "JOIN", "TRAIN" };
 	static int item_current_idx = 0;
-	static int last_item_current_idx = 5;
+	static int last_item_current_idx = 4;
 
 	// Here our selection data is an index.
 	const char* combo_label = items[item_current_idx];  // Label to preview before opening the combo (technically it could be anything)
@@ -1995,7 +1995,7 @@ static void gui_display_content()
 
 	ImGui::Combo("", &item_current_idx, items, IM_ARRAYSIZE(items));
 
-	if (last_item_current_idx == 5 && gui_state != GuiState::Replays)
+	if (last_item_current_idx == 4 && gui_state != GuiState::Replays)
 	{
 		// set default offline delay to 0
 		config::Delay = 0;
@@ -2030,6 +2030,7 @@ static void gui_display_content()
 		config::DojoEnable = false;
 	}
 	// RECEIVE menu option
+	/*
 	else if (item_current_idx == 4)
 	{
 		config::DojoEnable = true;
@@ -2039,8 +2040,8 @@ static void gui_display_content()
 		config::Training = false;
 
 		config::DojoServerIP = "";
-	}
-	else if (item_current_idx == 5)
+	}*/
+	else if (item_current_idx == 4)
 	{
 		config::DojoEnable = false;
 		config::Training = false;
