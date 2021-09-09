80d1100abf51da31e5e59c171c4c313836e2a41a
Enrique Santos <me@eos.fyi>
2021-09-10 01:53:45 +0300

    set ggpo as default netplay method

--- core/cfg/option.cpp
+++ core/cfg/option.cpp
@@ -159,7 +159,7 @@ Option<bool> EnableSessionQuickLoad("EnableSessionQuickLoad", false, "dojo");
 OptionString GameEntry("GameEntry", "", "dojo");
 OptionString Quark("Quark", "", "dojo");
 Option<bool> PlayerSwitched("PlayerSwitched", false, "dojo");
-OptionString NetplayMethod("NetplayMethod", "Delay", "dojo");
+OptionString NetplayMethod("NetplayMethod", "GGPO", "dojo");
 
 Option<int> EnableMouseCaptureToggle ("EnableMouseCaptureToggle", false, "input");
 
