d14b1fda6edd91b3948ee4b559267305607b24b0
Enrique Santos <me@eos.fyi>
2021-10-22 03:59:09 +0300

    do not show public ip by default in ggpo connect screen

--- core/cfg/option.cpp
+++ core/cfg/option.cpp
@@ -166,7 +166,7 @@ Option<bool> PlayerSwitched("PlayerSwitched", false, "dojo");
 OptionString NetplayMethod("NetplayMethod", "GGPO", "dojo");
 OptionString NetSaveBase("NetSaveBase", "https://github.com/blueminder/flycast-netplay-savestates/raw/main/", "dojo");
 Option<bool> NetStartVerifyRoms("NetStartVerifyRoms", false, "dojo");
-Option<bool> ShowPublicIP("ShowPublicIP", true, "dojo");
+Option<bool> ShowPublicIP("ShowPublicIP", false, "dojo");
 
 Option<int> EnableMouseCaptureToggle ("EnableMouseCaptureToggle", false, "input");
 
